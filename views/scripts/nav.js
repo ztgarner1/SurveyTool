81a014245d7d43f0d0b73fd9ab5925f50d5cf836
Matthew <aydlettm@yahoo.com>
2021-03-09 16:09:47 -0500

    Setup nav bar

--- views/scripts/nav.js
+++ views/scripts/nav.js
@@ -1,6 +1,15 @@
-document.write('<header>\
-      <div class="navbar">\
-          <a href="/">Home</a>\
+document.write('\
+	<header>\
+		<div class="navbar">\
+			<a href="/">Home</a>\
+			<div class="login-container">\
+				<% if(user != null) { %>\
+					<a href="/profile">Profile</a>\
+					<form action="/logout?_method=DELETE" method="POST">\
+						<button type="submit">Log Out</button>\
+					</form>\
+				<% } %>\
+			</div>\
         </div>\
     </header>\
 ');
