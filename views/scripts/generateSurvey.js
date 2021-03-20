54d2047ef064f431c0005061331cf079f0ec6ac1
Matthew <aydlettm@yahoo.com>
2021-03-20 18:20:22 -0400

    Update generateSurvey.js

--- views/scripts/generateSurvey.js
+++ views/scripts/generateSurvey.js
@@ -28,6 +28,6 @@ function genHTML() {
 
 function checkText() {
 	addQuestion("text", "THIS IS A TEST!", ["A", "B"]);
-	//document.getElementById("quesbutton").innerText = "TESTTJGBNDJBNFJ";
+	document.getElementById("quesbutton").innerText = "TESTTJGBNDJBNFJ";
 	genHTML();
 }
