4dc712404c332c24562584d9883feb38cb705702
Matthew <aydlettm@yahoo.com>
2021-03-20 18:11:05 -0400

    Update generateSurvey.js

--- views/scripts/generateSurvey.js
+++ views/scripts/generateSurvey.js
@@ -1,11 +1,11 @@
 
-//const quesSec;
-//
-//var surveyQues = [];
-//
-////Type is the type of question (text answer, multiple choice, selection, etc)
-////Question is the question its asking
-////Answers is an array of answers, depending on type will depend on how many answers
+const quesSec;
+
+var surveyQues = [];
+
+//Type is the type of question (text answer, multiple choice, selection, etc)
+//Question is the question its asking
+//Answers is an array of answers, depending on type will depend on how many answers
 //function addQuestion(questype, question, answers) {
 //	var temp = new Object();
 //	temp.type = questype;
@@ -13,18 +13,18 @@
 //	temp.ans = answers;
 //	surveyQues.push(temp);
 //}
-//
-//function genHTML() {
-//	var i;
-//	for (i = 0; i < surveyQues.length; i++) {
-//		
-//		if (surveyQues[i].type == "text") {
-//			var tempStr = "<h4>" + surveyQues[i].ques + "</h4>";
-//		}
-//		quesSec.innerHTML = tempStr;
-//	}
-//	
-//}
+
+function genHTML() {
+	var i;
+	for (i = 0; i < surveyQues.length; i++) {
+		
+		if (surveyQues[i].type == "text") {
+			var tempStr = "<h4>" + surveyQues[i].ques + "</h4>";
+		}
+		quesSec.innerHTML = tempStr;
+	}
+	
+}
 
 function checkText() {
 	//addQuestion("text", "THIS IS A TEST!", ["A", "B"]);
