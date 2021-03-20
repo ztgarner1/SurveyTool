ae7556a63ac00ddd3f0e851abcdeff9e2f724ddd
Matthew <aydlettm@yahoo.com>
2021-03-20 16:59:38 -0400

    Testing dynamic

---
+++ views/scripts/generateSurvey.js
@@ -0,0 +1,27 @@
+
+const quesSec = document.getElementById("questionsSections");
+
+var surveyQues = [];
+
+//Type is the type of question (text answer, multiple choice, selection, etc)
+//Question is the question its asking
+//Answers is an array of answers, depending on type will depend on how many answers
+function addQuestion(questype, question, answers) {
+	var temp = new Object();
+	temp.type = questype;
+	temp.ques = question;
+	temp.ans = answers;
+	surveyQues.push(temp);
+}
+
+function genHTML() {
+	var i;
+	for (i = 0; i < surveyQues.length; i++) {
+		
+		if (surveyQues[i].type == "text") {
+			var tempStr = "<h4>" + surveyQues[i].ques + "</h4>";
+		}
+		quesSec.innerHTML = tempStr;
+	}
+	
+}
