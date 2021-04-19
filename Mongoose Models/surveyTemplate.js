bce0b6e838e4e1ca718e7b22bfa9fb808fc0f6c1
Matthew <aydlettm@yahoo.com>
2021-04-19 15:01:39 -0400

    Update surveyTemplate.js

--- Mongoose Models/surveyTemplate.js
+++ Mongoose Models/surveyTemplate.js
@@ -4,7 +4,7 @@ const SurveyTemplate = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     title:String,
     questions:Array,
-	weight:int,
+	weight:Number,
 })
 
 module.exports = mongoose.model("SurveyTemplate", SurveyTemplate);
